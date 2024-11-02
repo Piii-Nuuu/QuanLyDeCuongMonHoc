b12ba639f21c5e5bfc879a9b6a9ec538090ea0b6
Trương Văn Bảo <truongvanbao2035@gmail.com>
2024-11-02 23:45:28 +0700

    create connection to database

--- Form1.cs
+++ Form1.cs
@@ -22,6 +22,7 @@ namespace QuanLyDeCuongProject
         
         private void Form1_Load(object sender, EventArgs e)
         {
+            // test push
             DataBase database = new DataBase(@"MSI\\MSSQLSERVER01");
         }
     }
