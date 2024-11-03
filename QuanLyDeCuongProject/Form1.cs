0fa876a10e635bdc257fe662a1b2d47b2bb6b95c
Trương Văn Bảo <truongvanbao2035@gmail.com>
2024-11-03 15:52:08 +0700

    change server name

--- QuanLyDeCuongProject/Form1.cs
+++ QuanLyDeCuongProject/Form1.cs
@@ -22,6 +22,7 @@ namespace QuanLyDeCuongProject
         { 
             // Enter Your Server Name
             // Hello Aliens @@
+            // Branch TanDat/241108....
             DataBase database = new DataBase(@""); 
         }
     }
