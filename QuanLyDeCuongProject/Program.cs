0f62d14fea2f53fe161b48f49efed24e1094952e
Trương Văn Bảo <truongvanbao2035@gmail.com>
2024-11-08 22:49:22 +0700

    up

--- QuanLyDeCuongProject/Program.cs
+++ QuanLyDeCuongProject/Program.cs
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace QuanLyDeCuongProject
-{
-    internal static class Program
-    {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new QuanLyPhanQuyen());
-        }
-    }
-}
-=======
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -44,4 +20,4 @@ namespace QuanLyDeCuongProject
         }
     }
 }
->>>>>>> eb82de20232b3b6f7066bd29bb51bb2b17b65f43
+
