0a450e3aba370f42fa2ce74181e9d8479323c4f1
Trương Văn Bảo <truongvanbao2035@gmail.com>
2024-11-09 01:01:56 +0700

    add .

--- QuanLyDeCuongProject/Program.cs
+++ QuanLyDeCuongProject/Program.cs
@@ -16,7 +16,7 @@ namespace QuanLyDeCuongProject
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new QL_Nganh());
         }
     }
 }
