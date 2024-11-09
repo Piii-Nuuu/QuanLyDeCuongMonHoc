139ae7240dc8b03b1f32a5c735c8eafb7aef8d3a
Ngoc Nu <nguyenthingocnu25022000@gmail.com>
2024-11-08 17:02:29 -0800

    est git

--- QuanLyDeCuongProject/Form1.cs
+++ QuanLyDeCuongProject/Form1.cs
@@ -22,6 +22,8 @@ namespace QuanLyDeCuongProject
         { 
             // Enter Your Server Name
             // Hello Aliens @@
+            // test  github ac
+
             DataBase database = new DataBase(@"SerVer Name"); 
         }
     }
