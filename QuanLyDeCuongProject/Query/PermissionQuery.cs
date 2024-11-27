287f71b996d9be672201cb3c3c53345050660dd0
baoyzy <nguyenlebaovyyy@gmail.com>
2024-11-27 12:22:30 +0700

    suaquyen

--- QuanLyDeCuongProject/Query/PermissionQuery.cs
+++ QuanLyDeCuongProject/Query/PermissionQuery.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace QuanLyDeCuongProject.Queries
 {
@@ -25,6 +26,10 @@ namespace QuanLyDeCuongProject.Queries
         public DataTable GetAllPermissionByQuyenId(string quyenId)
         {
             return database.ExecuteQuery($"select * from Permission p, Quyen_Permission qp, Quyen q where p.MaPermission = qp.MaPermission and qp.MaQuyen = q.MaQuyen and q.MaQuyen = {quyenId}");
+        }
+        public void CapNhat_Quyen(string mapermission, string maquyen, string maPermissonNew)
+        {
+             database.ExecuteNonQuery("update Quyen_Permission set MaPermission=" + maPermissonNew + " where MaPermission = " + mapermission + " and MaQuyen = " + maquyen + "");
         }
     }
 }
