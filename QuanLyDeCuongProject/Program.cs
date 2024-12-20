83a55b06ccf1f36e1bbec1b8c133e4b750abf3a9
Your Name <ngogiatun873@gmail.com>
2024-12-20 09:07:45 +0700

    ;

--- QuanLyDeCuongProject/Program.cs
+++ QuanLyDeCuongProject/Program.cs
@@ -18,7 +18,7 @@ namespace QuanLyDeCuongProject
         {   
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new DangNhap());
+            Application.Run(new DeCuong());
 
         }
     }
